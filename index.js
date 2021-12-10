334420f72191bad6950facd99505512f0a98d92e
ririErliza <ryantiadam@yahoo.co.uk>
2021-12-10 16:44:32 +0100

    working on js

---
+++ index.js
@@ -0,0 +1,21 @@
+//1
+let x = "John"
+let y = "Doe"
+console.log(x, "<>", y)
+
+//2
+whoever = {
+    name: "Riri",
+    surname: "Ryan",
+    email: "ryantiadam@yahoo.co.uk"
+}
+console.log(whoever)
+
+//3
+delete whoever.email;
+
+//4
+let arrayNew = ["rubble", "rocky", "skye", "zuma", "marshall", "chase", "everest", "tracker", "rex", "ella"]
+
+//5
+
